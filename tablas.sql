d73992588afa728c32a1f3309e9181e85a03e616
otreblan <otreblain@gmail.com>
2020-11-29 14:39:24 -0500

    First table

--- tablas.sql
+++ tablas.sql
@@ -1,1 +1,12 @@
--- TODO
+-- Tablas
+
+CREATE TABLE Empleado(
+	dni int,
+	nombre varchar(127),
+	nombre varchar(127),
+	celular int
+);
+
+-- Restricciones
+
+ALTER TABLE Empleado ADD PRIMARY KEY(dni);

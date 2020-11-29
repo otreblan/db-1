a6325797bb54542993e736ee21308b9b83226712
otreblan <otreblain@gmail.com>
2020-11-29 15:14:19 -0500

    tienda

--- tablas.sql
+++ tablas.sql
@@ -8,8 +8,8 @@ CREATE TABLE empleado(
 );
 
 CREATE TABLE gerente(
-	dni int,
-	direccion varchar(127),
+	dni int
+	-- direccion varchar(127), -- Redundante
 );
 
 CREATE TABLE vendedor(
@@ -30,13 +30,18 @@ CREATE TABLE trabaja(
 	direccion varchar(127)
 );
 
+CREATE TABLE tienda(
+	direccion varchar(127),
+	-- dni int -- Redundante
+);
+
 -- Restricciones
 
 ALTER TABLE empleado ADD PRIMARY KEY(dni);
 
 ALTER TABLE gerente ADD PRIMARY KEY(dni);
 ALTER TABLE gerente ADD FOREIGN KEY(dni) REFERENCES empleado(dni);
--- ALTER TABLE gerente ADD FOREIGN KEY(direccion) REFERENCES tienda(direccion);
+-- ALTER TABLE gerente ADD FOREIGN KEY(direccion) REFERENCES tienda(direccion); -- Redundante
 
 ALTER TABLE vendedor ADD PRIMARY KEY(dni);
 ALTER TABLE vendedor ADD FOREIGN KEY(dni) REFERENCES empleado(dni);
@@ -46,4 +51,6 @@ ALTER TABLE repartidor ADD FOREIGN KEY(dni) REFERENCES empleado(dni);
 
 ALTER TABLE trabaja ADD PRIMARY KEY(dni);
 ALTER TABLE trabaja ADD FOREIGN KEY(dni) REFERENCES empleado(dni);
--- ALTER TABLE trabaja ADD FOREIGN KEY(direccion) REFERENCES tienda(direccion);
+ALTER TABLE trabaja ADD FOREIGN KEY(direccion) REFERENCES tienda(direccion);
+
+ALTER TABLE tienda ADD PRIMARY KEY(direccion);

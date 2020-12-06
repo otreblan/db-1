622fb5ed3c92b87d0489c1b553f95cf532b66d02
otreblan <otreblain@gmail.com>
2020-12-06 16:56:46 -0500

    Generate gerentes

--- data.sh
+++ data.sh
@@ -18,6 +18,12 @@ function _tiendas()
 	cat <<< $tiendas
 }
 
+function _gerentes()
+{
+	echo "dni"
+	cat <<< $gerentes
+}
+
 n="${1:-100}"
 tienda_n=$((n/2))
 
@@ -27,7 +33,9 @@ dnis="$(cut -d, -f1 <<< $empleados)"
 
 vendedores="$(./vendedor.awk <<< $dnis)"
 tiendas="$(./tienda.awk "$tienda_n")"
+gerentes="$(shuf -n$tienda_n <<< $dnis)"
 
 _empleados > empleados.csv
 _vendedores > vendedores.csv
 _tiendas > tiendas.csv
+_gerentes > gerentes.csv

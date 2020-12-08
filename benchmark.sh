3739c9c7b3687549e4638a06cc27cfdca75388bd
otreblan <otreblain@gmail.com>
2020-12-08 16:14:43 -0500

    No yes

--- benchmark.sh
+++ benchmark.sh
@@ -6,10 +6,20 @@ function psql-explain()
 	local n=$1
 	shift
 
-	yes |\
-		head -n$n |\
-		awk -vcmd="$@" '{printf "-cEXPLAIN (ANALYZE, FORMAT JSON) %s;\0", cmd}' |\
+	awk \
+		-vcmd="$@" \
+		-vn=$n \
+		'BEGIN{
+			for(i = 0; i < n; i++){
+				printf "-cEXPLAIN (ANALYZE, FORMAT JSON) %s;\0", cmd;
+			}
+		}' |\
 		xargs -0 psql -tAUpostgres
 }
 
-psql-explain 10000 "SELECT * FROM gerente" | jq '.[]."Execution Time"'
+psql-explain 10000 '
+	SELECT t.direccion, g.dni
+	FROM trabaja AS t, gerente AS g
+	WHERE t.dni = g.dni
+	; ' |\
+	jq '.[]."Execution Time"'

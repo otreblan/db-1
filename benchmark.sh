d781bfa2faf94ee247f64b67863bb2db2e7fc165
otreblan <otreblain@gmail.com>
2020-12-08 16:56:25 -0500

    Wrap benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -19,42 +19,46 @@ function psql-explain()
 		jq '.[]."Execution Time"'
 }
 
-postfix="$1"
-n=${2:-100}
+function benchmark()
+{
+	postfix="$1"
+	n=${2:-100}
 
-psql-explain $n '
-	SELECT t.direccion, g.dni, e.nombre
-	FROM trabaja AS t, gerente AS g, empleado as e
-	WHERE t.dni = g.dni AND e.dni = g.dni
-	;' > query-1-"$postfix".csv
+	psql-explain $n '
+		SELECT t.direccion, g.dni, e.nombre
+		FROM trabaja AS t, gerente AS g, empleado as e
+		WHERE t.dni = g.dni AND e.dni = g.dni
+		;' > query-1-"$postfix".csv
 
-psql-explain $n '
-	SELECT t.direccion
-	FROM trabaja AS t, vendedor v
-	WHERE
-		t.dni = v.dni AND
-		v.ventas = (
-		SELECT MAX(ventas)
-		FROM vendedor
-	)
-	;' > query-2-"$postfix".csv
+	psql-explain $n '
+		SELECT t.direccion
+		FROM trabaja AS t, vendedor v
+		WHERE
+			t.dni = v.dni AND
+			v.ventas = (
+			SELECT MAX(ventas)
+			FROM vendedor
+		)
+		;' > query-2-"$postfix".csv
 
-psql-explain $n '
-	SELECT p.direccion, p.nombre, g.dni, MAX(p.precio) as max
-	FROM producto AS p, trabaja AS t, gerente as g
-	WHERE t.direccion = p.direccion AND t.dni = g.dni
-	GROUP BY p.direccion, p.nombre, g.dni
-	;' > query-3-"$postfix".csv
+	psql-explain $n '
+		SELECT p.direccion, p.nombre, g.dni, MAX(p.precio) as max
+		FROM producto AS p, trabaja AS t, gerente as g
+		WHERE t.direccion = p.direccion AND t.dni = g.dni
+		GROUP BY p.direccion, p.nombre, g.dni
+		;' > query-3-"$postfix".csv
 
-psql-explain $n '
-	SELECT m.direccion, v.dni
-	FROM (
-		SELECT t.direccion, MAX(v.ventas) AS ventas
-		FROM trabaja as t, vendedor as v
-		WHERE t.dni = v.dni
-		GROUP BY t.direccion
-	) AS m, trabaja AS t, vendedor as v
-	WHERE m.direccion = t.direccion AND
-	v.ventas = m.ventas
-	;' > query-4-"$postfix".csv
+	psql-explain $n '
+		SELECT m.direccion, v.dni
+		FROM (
+			SELECT t.direccion, MAX(v.ventas) AS ventas
+			FROM trabaja as t, vendedor as v
+			WHERE t.dni = v.dni
+			GROUP BY t.direccion
+		) AS m, trabaja AS t, vendedor as v
+		WHERE m.direccion = t.direccion AND
+		v.ventas = m.ventas
+		;' > query-4-"$postfix".csv
+}
 
+benchmark noindex 10

7f01da6d2f88a78b4025d962c9d49e644dbb6c0c
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2020-12-08 18:02:52 -0500

    Agregando Indices

--- index.sql
+++ index.sql
@@ -1,1 +1,23 @@
--- TODO
+-- Indices
+-- 1)
+CREATE INDEX index_trabaja_dni ON trabaja USING hash (dni);
+CREATE INDEX index_gerente_dni ON gerente USING hash (dni);
+CREATE INDEX index_empleado_dni ON empleado USING hash (dni);
+
+-- 2)
+--CREATE INDEX index_trabaja_dni ON trabaja USING hash (dni);
+CREATE INDEX index_vendedor_dni ON vendedor USING hash (dni);
+CREATE INDEX index_vendedor_ventas ON vendedor USING hash (ventas);
+
+-- 3)
+CREATE INDEX index_trabaja_direccion ON trabaja USING hash (direccion);
+CREATE INDEX index_producto_direccion ON producto USING hash (direccion);
+--CREATE INDEX index_trabaja_dni ON trabaja USING hash (dni);
+--CREATE INDEX index_gerente_dni ON gerente USING hash (dni);
+
+-- 4)
+--CREATE INDEX index_trabaja_dni ON trabaja USING hash (dni);
+--CREATE INDEX index_vendedor_dni ON vendedor USING hash (dni);
+
+CREATE INDEX index_trabaja_direccion ON trabaja USING hash (direccion);
+CREATE INDEX index_producto_direccion ON producto USING hash (direccion);
